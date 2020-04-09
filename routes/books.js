0ee7f0fd7a8f232da6d7ca51f05ad417dd0a3688
Lucan F <dapps911@hotmail.com>
2020-04-09 00:51:30 -0400

    created index route and new_book.pug

--- routes/books.js
+++ routes/books.js
@@ -2,19 +2,47 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
 
-
+// Hnadler function to wrap routes and avoid repetition
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error) {
+      res.status(500).send(error);
+    }
+  }
+}
 /* GET a list of books */
-router.get('/', (req, res, next) => {
-
-  Book.findAll({order: [["createdAt", "DESC"]]}).then(function(books){
-    res.render("books/all_books", { books: books });
-  }).catch(function(error){
-      res.send(500, error);
-  });
+router.get('/', asyncHandler( async(req, res, next) => {
+  const books = await Book.findAll({order: [["year", "DESC"]]});
+  res.render("books/all_books", { books: books, title: "SQL Library Manager" });
+}));
 
 
+/* Create a new article form. */
+router.get('/new_book',  (req, res) => {
+  res.render("books/new_book", { books: {}, title: "New Book" });
+});
 
-})
+// Set up your server, middleware and routes
+  // At the very least, you will need the following routes:
+    // get / - Home route should redirect to the /books route.
+
+    // get /books - Shows the full list of books.
+
+    // get /books/new - Shows the create new book form.
+
+    // post /books/new - Posts a new book to the database.
+
+    // get /books/:id - Shows book detail form.
+
+    // post /books/:id - Updates book info in the database.
+
+    // post /books/:id/delete - Deletes a book. 
+
+    
+        /*Careful, this can’t be undone. 
+        It can be helpful to create a new “test” book to test deleting.*/
 
 
 module.exports = router;
