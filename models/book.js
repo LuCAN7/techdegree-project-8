0541ffc9a17edc7e87d20407f7d51d707eb67c66
Lucan F <dapps911@hotmail.com>
2020-06-07 23:56:11 -0400

    removed sequelize validation for genre and year properties

--- models/book.js
+++ models/book.js
@@ -8,9 +8,6 @@ module.exports = (sequelize) => {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
-        notNull: {
-          msg: 'Please provide a value for "title"',
-        },
         notEmpty: {
           // custom error message
           msg: 'Please provide a value for "title"',
@@ -21,9 +18,6 @@ module.exports = (sequelize) => {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
-        notNull: {
-          msg: 'Please provide a value for "author"',
-        },
         notEmpty: {
           // custom error message
           msg: 'Please provide a value for "author"',
@@ -32,29 +26,9 @@ module.exports = (sequelize) => {
     },
     genre: {
       type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for "genre"',
-        },
-        notEmpty: {
-          // custom error message
-          msg: 'Please provide a value for "genre"',
-        },
-      }
     },
     year: {
       type: Sequelize.INTEGER,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for "year"',
-        },
-        notEmpty: {
-          // custom error message
-          msg: 'Please provide a value for "year"',
-        },
-      }
     }
   }, { sequelize: sequelize });
 
